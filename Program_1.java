e8e4764d8e7bba702029226fc4e4a9b2e868b11d
Sai Divya <saidivyavooka@Sais-MacBook-Pro.local>
2022-11-08 17:07:38 -0500

    renaming the file name

--- Program_1.java
+++ Program_1.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 //Program Statement: Keep taking numbers as inputs till the user enters ‘x’, after that print sum of all.
 
+
 class Program_1 {  
     public static void main(String args[]){  
      String a="x";
