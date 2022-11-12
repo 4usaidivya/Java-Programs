207e8072b402c90aeee63f32c87e0002b949ef44
Sai Divya <saidivyavooka@Sais-MacBook-Pro.local>
2022-11-12 13:25:53 -0500

    program 5

--- Program_5.java
+++ Program_5.java
@@ -3,3 +3,13 @@
 //Problem Statement: Take name as input and print a greeting message for that particular name.
 // Time complexity : O(1)
 // Space complexity : O(1)
+import java.util.*;
+public class Program_5{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.printf("Enter your name:");
+        System.out.println("Hello "+input.nextLine()+"!!!");
+
+    }
+
+}
