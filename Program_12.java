3c834e04fec49908ab10ecf52107628cceba89d2
Sai Divya <saidivyavooka@Sais-MacBook-Pro.local>
2022-11-13 20:09:33 -0500

    program 12 is added

---
+++ Program_12.java
@@ -0,0 +1,39 @@
+
+//Problem statement: To find Armstrong Number between two given number.
+//Time Complexity:O(size of number of digits in a number)
+//Space Complexity:O(1)
+
+//Output
+//Case 1
+// Enter the number to check for armstrong number:
+// 1634
+// 1634 is a Armstrong number.
+
+//case 2
+// Enter the number to check for armstrong number:
+// 120
+// 120 is not a Armstrong number.
+
+import java.util.*;
+public class Program_12 {
+   public static void main(String[] args) {
+    int n,temp,a,b;
+    int count=0;
+    Scanner input = new Scanner(System.in);
+    System.out.println("Enter the number to check for armstrong number:");
+    n=input.nextInt();
+    temp =n;
+    b= (int)Math.log10(n) + 1; //To find no of digits in the number
+
+    while(n!=0){
+        a=n%10;
+        count=(int) (count+Math.pow(a, b));
+        n=n/10;
+    }
+    if(temp==count){
+        System.out.println(temp+" is a Armstrong number.");
+    }else{
+        System.out.println(temp+" is not a Armstrong number.");
+    }
+   } 
+}
