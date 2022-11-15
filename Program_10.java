24d5b8a2835ce6741b6626c3fbee0c8ff101f174
Sai Divya <saidivyavooka@Sais-MacBook-Pro.local>
2022-11-15 11:48:17 -0500

    program 10 is updated

--- Program_10.java
+++ Program_10.java
@@ -1,32 +1,32 @@
 
-//Problem Statement: To calculate Fibonacci Series up to n numbers.
+//Problem Statement: To calculate Fibonacci Series up to n number.
 //Time complexity:O(N)
 //Space complexity:O(1)
 
 //Output
-//Enter the number for fibonacci series: 
-// 10
-// The fibonacci series upto 10 numbers
-// 0 1 1 2 3 5 8 13 21 34
+// Enter the number for fibonacci series: 
+// 7
+// The fibonacci series upto 7 numbers
+// Fibonaci series of 7th digit: 8
 
 import java.util.*;
 public class Program_10 {
     public static void main(String[] args) {
         int a=0;
         int b=1;
-        int c;
+        int c=0;
         int n;
         Scanner input = new Scanner(System.in);
         System.out.println("Enter the number for fibonacci series: ");
         n = input.nextInt();
         System.out.println("The fibonacci series upto "+ n +" numbers");
-        for(int i=0;i<n;i++){
-            System.out.print(a + " ");
+        for(int i=2;i<n;i++){
             c=a+b;
             a=b;
             b=c;
            
-        }    
+        } 
+        System.out.println("Fibonaci series of "+n+"th digit: "+c);   
 
        
     }
